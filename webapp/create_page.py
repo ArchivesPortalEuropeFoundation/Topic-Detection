ea82869826f9796aabbe9e2a5a768be01a0958e8
fedenanni <nanni.federico@gmail.com>
2021-05-24 17:32:58 +0100

    Setup API

--- webapp/create_page.py
+++ webapp/create_page.py
@@ -40,6 +40,34 @@ def my_form_post():
             return flask.render_template('my-form.html',langs=langs,message="Entity mentions not found in corpus!")
     return flask.render_template('my-form.html',  tables=[ranking.to_html(classes='data',index=False)], titles=ranking.columns.values,langs=langs)
 
+
+
+@APP.route('/query', methods=['GET'])
+def query_api():
+
+    # we load the dataset
+
+    text = flask.request.args['text']
+    lang = flask.request.args['lang']
+    search_type = flask.request.args['type']
+    n_res = int(flask.request.args['n_res'])
+    if search_type == "concept":
+        query_emb = nlp.text_embedding(text,lang)
+        if query_emb:
+            ranking= nlp.concept_search(index,query_emb,labels,doc_names,texts,n_res)
+        else:
+            return "Concept not found in embedding space!"
+
+    if search_type == "entity":
+        #for the moment hardcoded
+        allow_partial_match = True
+        ranking = nlp.entity_search(text,lang,labels,doc_names,texts,n_res,langs,allow_partial_match)
+        if ranking.empty:
+            return "Entity mentions not found in corpus!"
+    return ranking.to_html(classes='data',index=False)
+
+
+
 if __name__ == '__main__':
 
     # we load the dataset

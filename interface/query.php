10735219ed1471cb6af8b2d5313a7d00faa8ae50
Federico Nanni <fedenanni@users.noreply.github.com>
2021-06-19 14:51:12 +0100

    Update query.php

--- interface/query.php
+++ interface/query.php
@@ -9,7 +9,7 @@ $_SESSION["text"] = $_POST['text'];
 $_SESSION["n_res"] = $_POST['n_res'];
 $_SESSION["broad_entity_search"] = isset($_POST['broad_entity_search']) ? "True" : "False";
 
-$url= 'http://127.0.0.1:5000/query?';
+$url= 'http://127.0.0.1:6000/query?';
 
 $data = array('lang' => $_SESSION['lang'],
               'type' => $_SESSION['type'],

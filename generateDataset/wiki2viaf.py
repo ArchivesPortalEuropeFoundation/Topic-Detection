f30359a89a8e8dffa4569664202c539040e968ca
fedenanni <nanni.federico@gmail.com>
2021-12-18 12:03:53 +0000

    clean up

---
+++ generateDataset/wiki2viaf.py
@@ -0,0 +1,16 @@
+import json
+
+wiki2viaf = {}
+
+with open("/Users/fnanni/Downloads/viaf-20210802-links.txt", "r") as r:
+    for line in r:
+        if "wiki" in line.lower():
+            try:
+                viaf = line.split("\t")[0].strip().replace("\n","").replace("\t","").replace("\r","")
+                wiki = line.split("\t")[1].split("@")[1].strip().replace("\n","").replace("\t","").replace("\r","")
+                wiki2viaf[wiki] = viaf
+            except Exception as e:
+                print (e,line)
+
+with open('../webapp/data/wiki2viaf.json', 'w') as fp:
+    json.dump(wiki2viaf, fp)

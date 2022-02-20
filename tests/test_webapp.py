ed885bc362044f440563ec59d7fa9233354ee8b6
fedenanni <nanni.federico@gmail.com>
2022-02-20 12:30:52 +0000

    Add pytest example in github action

---
+++ tests/test_webapp.py
@@ -0,0 +1,13 @@
+import requests
+
+def test_basic_query():
+
+    lang = 'fr'
+    type = 'concept'
+    text = 'et'
+    n_res = '10'
+    broad_entity_search = 'False'
+    boolean_search = 'False'
+
+    r = requests.get('http://0.0.0.0:5000/query?lang='+lang+'&type='+type+'&text='+text+'&n_res='+n_res+'&broad_entity_search='+broad_entity_search+'&boolean_search='+boolean_search)
+    assert r.status_code == 200

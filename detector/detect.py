46a72a3e2365c64d5dd592743427ca20604d3394
fedenanni <nanni.federico@gmail.com>
2022-06-25 11:59:19 +0100

    Add test version of detect

--- detector/detect.py
+++ detector/detect.py
@@ -1,5 +1,6 @@
 import os
 import sys
+
 import pandas as pd
 from flair.data import Sentence
 from flair.models import SequenceTagger
@@ -8,22 +9,30 @@ from flair.models import SequenceTagger
 sys.path.insert(0, os.path.abspath(os.path.pardir))
 from utils import nlp
 
+
 # load tagger
-en_tagger = SequenceTagger.load("flair/ner-english")
-de_tagger = SequenceTagger.load("flair/ner-german")
-fr_tagger = SequenceTagger.load("flair/ner-french")
-nl_tagger = SequenceTagger.load("flair/ner-dutch")
+def load_tagger(test=False):
+    if test is True:
+        en_tagger = SequenceTagger.load("flair/ner-english-fast")
+        return {"en": en_tagger}
 
-ner_dict = {"en": en_tagger, "de": de_tagger, "fr": fr_tagger, "nl": nl_tagger}
+    en_tagger = SequenceTagger.load("flair/ner-english")
+    de_tagger = SequenceTagger.load("flair/ner-german")
+    fr_tagger = SequenceTagger.load("flair/ner-french")
+    nl_tagger = SequenceTagger.load("flair/ner-dutch")
+
+    ner_dict = {"en": en_tagger, "de": de_tagger, "fr": fr_tagger, "nl": nl_tagger}
+    return ner_dict
 
 
-def tag_string(text: str, lang: str) -> [str]:
+def tag_string(ner_dict: dict, text: str, lang: str) -> [str]:
     """
     Given a string and a language returns the urls of the identified entities
 
     Args:
+        ner_dict (dict): A dict with the loaded models
         text (str): A string of text
-        lang (str): a language (either en,de,fr,nl)
+        lang (str): a language (either en,de,fr,nl - only en for test)
     Returns:
         [str]: A list containing the urls of the identified entities
     """
@@ -41,15 +50,9 @@ def tag_string(text: str, lang: str) -> [str]:
     for entity in sentence.get_spans("ner"):
         print(entity.text)
         url = nlp.get_url(entity.text, lang)
-        str_url = "<a href="+url+">"+entity.text+"</a>"
+        str_url = "<a href=" + url + ">" + entity.text + "</a>"
         ents.append(str_url)
     headers = ["Content", "Entities"]
     df = pd.DataFrame([[text, ents]], columns=headers)
 
     return df
-
-
-# make example sentence
-text = "Berlin ist eine schöne Stadt mit dem Deutschen Bundestag"
-lang = "de"
-print(tag_string(text, lang))
